dfb9aee533577ef61c1fdc7a6a6974843abbdd70
mrk134 <41437453+mrk134@users.noreply.github.com>
2020-06-30 09:00:21 +0200

    Update template.tf

--- template.tf
+++ template.tf
@@ -29,6 +29,7 @@ resource "azurerm_network_interface" "main" {
     name                          = "${var.prefix}-ipconfiguration"
     subnet_id                     = "${azurerm_subnet.internal.id}"
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = "${azurerm_public_ip.test.id}"
   }
   tags = {"Purpose" = "Terraformdemo"}
 }
